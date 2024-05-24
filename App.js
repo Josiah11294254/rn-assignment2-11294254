8f46c0cf0fc1457064c7236926b5b5aa97b87a51
unknown <minwidth49@gmail.com>
2024-05-24 15:36:14 +0000

    changed view component background

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My name is </Text>
+      <Text style={styles.name}>Josiah</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#0a1620",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  
 });
